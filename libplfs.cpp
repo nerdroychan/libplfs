a285eba28aa7835a56042d91d4c3da03c29f1267
Chen Chen <mail@roychan.org>
2017-11-02 21:02:52 +0800

    fread() has unresolved bugs

--- libplfs.cpp
+++ libplfs.cpp
@@ -64,16 +64,17 @@ void print_tables() {
 */
 
 char* normalize_path(const char* path) {
-    char* tmp_path = (char*)malloc(sizeof(char)*strlen(path));
-    strncpy(tmp_path, path, strlen(path));
-    char* real_path = realpath(tmp_path, NULL);
-    char* t = tmp_path + strlen(path) - 1;
-    while (real_path == NULL && t >= tmp_path) {
-        while (*t != '/' && t >= tmp_path) t--;
-        *t = '\0';
-        real_path = realpath(tmp_path, NULL);
-    }
-    return real_path;
+    // char* tmp_path = (char*)malloc(sizeof(char)*strlen(path));
+    // strncpy(tmp_path, path, strlen(path));
+    // char* real_path = realpath(tmp_path, NULL);
+    // char* t = tmp_path + strlen(path) - 1;
+    // while (real_path == NULL && t >= tmp_path) {
+    //     while (*t != '/' && t >= tmp_path) t--;
+    //     *t = '\0';
+    //     real_path = realpath(tmp_path, NULL);
+    // }
+    // return real_path;
+    return realpath(path, NULL);
 }
 
 
@@ -95,13 +96,13 @@ unsigned long string_hash(const char *str) {
 // * absolute path
 // * plfs path
 FILE* normalized_plfs_open(const char* path, int oflags, mode_t mode) {
-    dstream << "= Normalized plfs open: " << path << endl;
+    dstream << "Enter normalized_plfs_open()" << endl;
     Plfs_fd* plfs_fd = NULL;
     unsigned long hashed_real_path = string_hash(path);
     bool first_open = true;
     if (path_file_table.count(hashed_real_path) != 0) {
         plfs_fd = path_file_table[hashed_real_path]->plfs_fd; // Not the first time to open the file
-        dstream << "- Not first opening; current ref_num " << path_file_table[hashed_real_path]->ref_num << endl;
+        dstream << "Not first opening; current ref_num is " << path_file_table[hashed_real_path]->ref_num << endl;
         first_open = false;
     }
     plfs_error_t err = PLFS_EAGAIN;
@@ -110,11 +111,11 @@ FILE* normalized_plfs_open(const char* path, int oflags, mode_t mode) {
         err = plfs_open(&plfs_fd, path, oflags, getpid(), mode, NULL);
     }
     if (err == PLFS_SUCCESS) {
-        dstream << "- Open PLFS file succeed! plfs_fd = " << plfs_fd << endl;
+        dstream << "plfs_open() succeed! plfs_fd = " << plfs_fd << endl;
         FILE* file = tmpfile();
         if (file != NULL) {
             int fake_fd = fileno(file);
-            dstream << "- Open Fake file succeed! fake_fd = " << fileno(file) << ", FILE " << file  << endl;
+            dstream << "tmpfile() succeed! fake_fd = " << fileno(file) << ", FILE " << file  << endl;
             Plfs_file* plfs_file;
             if (first_open) {
                 plfs_file = (Plfs_file*)malloc(sizeof(struct Plfs_file));
@@ -133,14 +134,14 @@ FILE* normalized_plfs_open(const char* path, int oflags, mode_t mode) {
         }
         else {
             int t_ref = 0;
-            dstream << "- Fake file open/create failed" << endl;
+            dstream << "Fake file open/create failed" << endl;
             plfs_close(plfs_fd, getpid(), getuid(), oflags, NULL, &t_ref);
             ret = NULL;
         }
     }
     else {
         errno = plfs_error_to_errno(err);
-        dstream << "- plfs_open failed, err = " << errno << endl;
+        dstream << "plfs_open() failed" << endl;
         ret = NULL;
     }
     return ret;
@@ -149,7 +150,7 @@ FILE* normalized_plfs_open(const char* path, int oflags, mode_t mode) {
 
 int normalized_plfs_close(FILE* file) {
     int fd = fileno(file);
-    dstream << "Trying to close file. fake_fd = " << fd << endl;
+    dstream << "Enter normalized_plfs_close()" << endl;
     int ret;
     Plfs_file* plfs_file = fd_file_table[fd];
     Plfs_fd* plfs_fd = plfs_file->plfs_fd;
@@ -160,28 +161,26 @@ int normalized_plfs_close(FILE* file) {
     }
     if (err != PLFS_SUCCESS) {
         errno = plfs_error_to_errno(err);
-        dstream << "- Close PLFS file failed, err = " << err << endl;
+        dstream << "plfs_close() failed" << endl;
         ret = -1;
     }
     else {
-        dstream << "- Close PLFS file succeed!" << endl;
+        dstream << "plfs_close() succeed!" << endl;
         ret = fclose(file);
         if (ret == 0) {
-            dstream << "- Close fake file succeed!" << endl;
+            dstream << "fclose() succeed!" << endl;
             fd_file_table.erase(fd);
             if (plfs_file->ref_num == 0) {
-                dstream << "- Ref = 0, delete the PLFS file" << endl;
+                dstream << "Ref = 0, delete the PLFS file" << endl;
                 path_file_table.erase(plfs_file->hashed_real_path);
                 free(plfs_file);
             }
         }
         else {
-            dstream << "- Close fake file failed, it returns " << ret << endl;
-            cout << "FATAL inconsistency occurs, exit!!" << endl;
+            dstream << "fclose() failed (FATAL ERROR)" << endl;
             exit(-1);
         }
     }
-    dstream << "close() returns " << ret << endl;
     return ret;
 }
 
@@ -193,8 +192,8 @@ int normalized_plfs_close(FILE* file) {
 */
 
 int _open(const char *path, int oflags, mode_t mode) {
-    dstream << "Call open on path " << path << " with oflags " << oflags << " and mode " << mode << endl;
-    char* real_path = realpath(path, NULL);
+    dstream << "Call open() on path " << path << " with oflags " << oflags << " and mode " << mode << endl;
+    char* real_path = normalize_path(path);
     if (real_path == NULL) {
         dstream << "Invalid path, return -1" << endl;
         return -1;
@@ -206,12 +205,13 @@ int _open(const char *path, int oflags, mode_t mode) {
     }
     FILE* file = normalized_plfs_open(real_path, oflags, mode);
     if (file == NULL) {
-        dstream << " - Normalized open failed" << endl;
+        dstream << "normalized_plfs_open() failed" << endl;
         return -1;
     }
     int fake_fd = fileno(file);
     fd_cfile_table[fake_fd] = file;
     free(real_path);
+    dstream << "open() returns " << fake_fd << endl;
     return fake_fd;
 }
 
@@ -235,13 +235,13 @@ int _open(const char *path, int oflags) {
 int _close(int fd) {
     dstream << "Trying to close file. fake_fd = " << fd << endl;
     if (fd_cfile_table.count(fd) == 0) {
-        dstream << "- File descriptor not found, return standard close()" << endl;
+        dstream << "File descriptor not found, return standard close()" << endl;
         return close(fd);
     }
     FILE* file = fd_cfile_table[fd];
     int ret = normalized_plfs_close(file);
     if (ret != 0) {
-        dstream << " - Normalized close failed" << endl;
+        dstream << "normalized_plfs_close() failed" << endl;
         return -1;
     }
     fd_cfile_table.erase(fd);
@@ -259,7 +259,7 @@ int _close(int fd) {
 ssize_t _read(int fd, void *buf, size_t nbytes) {
     dstream << "Trying to read file. fake_fd = " << fd << endl;
     ssize_t ret;
-    if (fd_file_table.count(fd) == 0) {
+    if (fd_cfile_table.count(fd) == 0) {
         dstream << "- Invalid file descriptor, return" << endl;
         ret = read(fd, buf, nbytes);
     }
@@ -295,7 +295,7 @@ ssize_t _read(int fd, void *buf, size_t nbytes) {
 ssize_t _write(int fd, const void *buf, size_t nbytes) {
     dstream << "Trying to write file. fake_fd = " << fd << endl;
     ssize_t ret;
-    if (fd_file_table.count(fd) == 0) {
+    if (fd_cfile_table.count(fd) == 0) {
         dstream << "- Invalid file descriptor, return" << endl;
         ret = write(fd, buf, nbytes);
     }
@@ -324,7 +324,7 @@ ssize_t _write(int fd, const void *buf, size_t nbytes) {
 
 
 int _chmod(const char *path, mode_t mode) {
-    char* real_path = realpath(path, NULL);
+    char* real_path = normalize_path(path);
     int ret;
     if (real_path == NULL) ret = -1;
     else if (is_plfs_path(real_path) == 1) {
@@ -370,7 +370,7 @@ int _chmod(const char *path, mode_t mode) {
 
 
 int _access(const char* path, int mask) {
-    char* real_path = realpath(path, NULL);
+    char* real_path = normalize_path(path);
     int ret;
     if (real_path == NULL) ret = -1;
     else if (is_plfs_path(real_path) == 1) {
@@ -393,7 +393,7 @@ int _access(const char* path, int mask) {
 
 
 int _chown(const char *path, uid_t uid, gid_t gid) {
-    char* real_path = realpath(path, NULL);
+    char* real_path = normalize_path(path);
     int ret;
     if (real_path == NULL) ret = -1;
     else if (is_plfs_path(real_path) == 1) {
@@ -465,7 +465,7 @@ int _creat(const char* path, mode_t mode) {
 
 FILE* _fopen(const char *path, const char* mode) {
     dstream << "Call open on path " << path << " with mode " << mode << endl;
-    char* real_path = realpath(path, NULL);
+    char* real_path = normalize_path(path);
     if (real_path == NULL) {
         dstream << "Invalid path, return -1" << endl;
         return NULL;
@@ -487,24 +487,73 @@ FILE* _fopen(const char *path, const char* mode) {
 }
 
 
-int main() {
-    int a = _open("/mnt/PLFS/test", O_RDWR);
-    dstream << a << endl;
-    print_tables();
-    int b = _open("/mnt/PLFS/test", O_RDWR);
-    print_tables();
-    int c = _open("../../../../../mnt/PLFS/test", O_RDWR);
-    print_tables();
-    int d = _open("/mnt/PLFS/test2", O_RDONLY);
-    print_tables();
+int _fclose(FILE* file) {
+    dstream << "Call close on file" << file << endl;
+    int fd = fileno(file);
+    if (fd_file_table.count(fd) == 0) {
+        dstream << "File descriptor not found, return standard fclose()" << endl;
+        return fclose(file);
+    }
+    int ret = normalized_plfs_close(file);
+    if (ret != 0) {
+        dstream << "normalized_plfs_close() failed" << endl;
+        return -1;
+    }
+    dstream << "fclose() returns " << ret << endl;
+    return ret;
+}
 
-    _close(a);
-    print_tables();
-    _close(b);
-    print_tables();
-    _close(c);
-    print_tables();
-    _close(d);
+
+size_t _fread(void* ptr, size_t size, size_t n, FILE* stream) {
+    ssize_t ret;
+    int fd = fileno(stream);
+    dstream << "Trying to read file. fake_fd = " << fd << endl;
+    if (fd_file_table.count(fd) == 0) {
+        dstream << "Not in table, return normal fread()" << endl;
+        ret = fread(ptr, size, n, stream);
+    }
+    else {
+        dstream << "File descriptor found in global fd table" << endl;
+        Plfs_file* plfs_file = fd_file_table[fd];
+        long offset = ftell(stream);
+        dstream << "Current offset = " << offset;
+        plfs_error_t err = PLFS_EAGAIN;
+        while (err == PLFS_EAGAIN) {
+            err = plfs_read(plfs_file->plfs_fd, (char*)ptr, n*size, offset, &ret);
+        }
+        if (err == PLFS_SUCCESS) {
+            dstream << "File read succeed, read " << ret << " units" << endl;
+            fseek(stream, ret, SEEK_CUR);
+            dstream << "New offset is " << ftell(stream) << endl;
+        }
+        else {
+            errno = plfs_error_to_errno(err);
+            dstream << "- PLFS read failed, it returns " << err;
+        }
+    }
+    dstream << "fread() returns " << ret << endl;
+    return ret;
+}
+
+
+int main() {
+    // int a = _open("/mnt/PLFS/test", O_RDWR);
+    // dstream << a << endl;
+    // print_tables();
+    // int b = _open("/mnt/PLFS/test", O_RDWR);
+    // print_tables();
+    // int c = _open("../../../../../mnt/PLFS/test", O_RDWR);
+    // print_tables();
+    // int d = _open("/mnt/PLFS/test2", O_RDONLY);
+    // print_tables();
+
+    // _close(a);
+    // print_tables();
+    // _close(b);
+    // print_tables();
+    // _close(c);
+    // print_tables();
+    // _close(d);
 
     // // char buf[5] = {'0', '1', 'c', 'x', '\n'};
     // // for (int i=0; i<1024; i++) {
@@ -520,6 +569,29 @@ int main() {
     // cout << str_to_oflags("w+b") << (O_RDWR | O_TRUNC | O_CREAT) << endl;
     // cout << str_to_oflags("ab") << (O_WRONLY | O_CREAT | O_APPEND) << endl;
     // cout << str_to_oflags("a+b") << (O_RDWR | O_CREAT | O_APPEND) << endl;
+    
+    FILE* a = _fopen("/mnt/PLFS/test", "w+");
+    print_tables();
+    // FILE* b = _fopen("/mnt/PLFS/test", "w+");
+    // print_tables();
+    // FILE* c = _fopen("../../../../../mnt/PLFS/test", "w+");
+    // print_tables();
+    // FILE* d = _fopen("/mnt/PLFS/test2", "w+");
+    // print_tables();
+
+    char buf[1024];
+    // memset(buf, 0, 1024*sizeof(char));
+    _fread(buf, 1, 1, a);
+    // cout << buf << endl;
+
+    // _fclose(a);
+    // print_tables();
+    // _fclose(b);
+    // print_tables();
+    // _fclose(c);
+    // print_tables();
+    // _fclose(d);
+    // print_tables();
 
     return 0;
 }
