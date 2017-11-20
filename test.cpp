350312bd69f28365a6790112af6229d97707a9a2
Roy Chan <i@roychan.org>
2017-11-20 15:15:18 +0800

    Finished normalize_path() function

--- test.cpp
+++ test.cpp
@@ -49,7 +49,7 @@ void gen_input(char*** input, int times) {
 }
 
 int main(int argc, char** argv) {
-    int times = 1000000;
+    int times = 1000;
     char** input = NULL;
     gen_input(&input, times);
 
@@ -63,25 +63,32 @@ int main(int argc, char** argv) {
     int b;
     gettimeofday(&start, NULL);
     // b = open("/mnt/plfs/t1", O_RDWR);
-    a = fopen("/mnt/plfs/t1", "w");
+    // a = fopen("/mnt/plfs/t1", "w+");
     //int fd = fileno(a);
     for (int i=0; i<times; i++) {
+        // if (i % 10 == 0) { printf("%d ", i); fflush(stdout); }
         // printf("%d ", i);
         // fflush(stdout);
         // a = fopen("/mnt/plfs/t1", "a+");
         // std::cout << lseek(b, 0, SEEK_CUR) << std::endl;
-        fwrite(input[i], sizeof(char), strlen(input[i]), a);
-//        write(fd, input[i], strlen(input[i]));
+        // a = fopen("/mnt/plfs/t1", "r");
+        // fwrite(input[i], sizeof(char), strlen(input[i]), a);
+        // fread(input[i], sizeof(char), 10, a);
+        // fclose(a);
+        b = open("/mnt/plfs/t1", O_RDWR);
+        // write(b, input[i], strlen(input[i]));
+        read(b, input[i], 10);
+        close(b);
         // fwrite(input2, sizeof(char), 1, a);
         // write(b, input[i], strlen(input[i]));
         
     }
     //close(fd);
-    fclose(a);
+    // fclose(a);
         // close(b); 
     gettimeofday(&stop, NULL);
     secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
-    printf("Time %fs\n",secs);
+    printf("\nTime %fs\n",secs);
     
     // int a = open("/mnt/plfs/1", 0644);
     // char buf[1024];
